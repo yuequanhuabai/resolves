d118a74f617ec49c3365853be76006d8ff3137ec
baiyingwaming <baiyingwaming@foxmail.com>
2026-01-08 09:37:03 +0800

    20260108

---
+++ error_record/20251009/buylist/buy_list_init_final_script.sql
@@ -0,0 +1,133 @@
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_BND_THM_1', N'Pvb BondList-Thematic1', 1, NULL, 2, N'', NULL, NULL, NULL, NULL,NULL, 0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_BND_THM_2', N'Pvb BondList-Thematic2', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_BND_THM_3', N'Pvb BondList-Thematic3', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_BND_TOP', N'Pvb Bond Top Focus List', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_EQ_THM_1', N'Pvb Equity Thematic 1', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_EQ_THM_2', N'Pvb Equity Thematic 2', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_EQ_THM_3', N'Pvb Equity Thematic 3', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_EQ_TOP', N'Pvb Equity Top Focus List', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_ALL_TOP', N'Pvb Focus List(All Assets)', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_FND_THM_1', N'PvB Fund Thematic 1', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_FND_THM_2', N'PvB Fund Thematic 2', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_FND_THM_3', N'PvB Fund Thematic 3', 1, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'LIST_FND_FOCUS', N'PvB Fund Top Focus List', 1, NULL, 2, N'', NULL, NULL, NULL, NULL,NULL, 0, current_timestamp, NULL, 0);
+GO
+
+-- retail banking
+
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_MPI_R1', N'Retail MPI list Risk Level 1', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_MPI_R2', N'Retail MPI list Risk Level 2', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_MPI_R3', N'Retail MPI list Risk Level 3', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_MPI_R4', N'Retail MPI list Risk Level 4', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_MPI_R5', N'Retail MPI list Risk Level 5', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_MPI_GNL', N'Retail MPI list General', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_NON_MPI_R1', N'Retail NON MPI Risk Level 1', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_NON_MPI_R2', N'Retail NON MPI Risk Level 2', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_NON_MPI_R3', N'Retail NON MPI Risk Level 3', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_NON_MPI_R4', N'Retail NON MPI Risk Level 4', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_NON_MPI_R5', N'Retail NON MPI Risk Level 5', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
+INSERT INTO buy_list
+(id, business_id, name, business_type, process_instance_id, approval_status , maker, maker_datetime, maker_business_date, checker, checker_datetime, checker_business_date, record_version, valid_start_datetime, valid_end_datetime, del_flag)
+VALUES
+    (newid(),N'RTLIST_NON_MPI_GNL', N'Retail NON MPI list General', 2, NULL, 2, N'', NULL, NULL, NULL, NULL, NULL,0, current_timestamp, NULL, 0);
+GO
